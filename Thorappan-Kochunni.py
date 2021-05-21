df08b13c457ccfa42ae977d97473a387be8934db
Salman Nazeer <salmanselu2002@gmail.com>
2021-05-21 20:39:04 +0530

    updated everything

--- Thorappan-Kochunni.py
+++ Thorappan-Kochunni.py
@@ -1,24 +1,98 @@
-########## DO NOT TOUCH ################################################
-import tweepy
-import time
-# NOTE: I put my keys in the keys.py to separate them
-# from this main file.
-# Please refer to keys_format.py to see the format.
-from keys import *
 
-# NOTE: flush=True is just for running this script
-# with PythonAnywhere's always-on task.
-# More info: https://help.pythonanywhere.com/pages/AlwaysOnTasks/
-print('this is my twitter bot', flush=True)
+import tweepy, time
+# Enter your keys/secrets as strings in the following fields
+
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
+ACCESS_KEY = ""
+ACCESS_SECRET = ""
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+
 api = tweepy.API(auth)
 ############################################################################
 
 
+def get_all_tweets(tweet):
+    screen_name = tweet.user.screen_name
+    lastTweetId = tweet.id
+    #initialize a list to hold all the tweepy Tweets
+    allTweets = []
+    #make initial request for most recent tweets (200 is the maximum allowed count)
+    new_tweets = api.user_timeline(screen_name = screen_name,count=200)
+    allTweets.extend(new_tweets)
+    #save the id of the oldest tweet less one
+    oldest = allTweets[-1].id - 1
+    #keep grabbing tweets until there are no tweets left to grab
+    while len(new_tweets) > 0 and oldest >= lastTweetId:
+        print(f"getting tweets before {oldest}")
+        #all subsiquent requests use the max_id param to prevent duplicates
+        new_tweets = api.user_timeline(screen_name = screen_name,count=200,max_id=oldest)
+        #save most recent tweets
+        allTweets.extend(new_tweets)
+        #update the id of the oldest tweet less one
+        oldest =allTweets[-1].id - 1
+        print(f"...{len(allTweets)} tweets downloaded so far")
+    outtweets = [tweet.id for tweet in allTweets]
+    return outtweets
 
+def getAllTweetsInThreadAfterThis(tweetId):
+    thread = []
+    hasReply = True
+    res = api.get_status(tweetId, tweet_mode='extended')
+    allTillThread = get_all_tweets(res)
+    thread.append(res)
+    if allTillThread[-1] > res.id:
+        print("Not able to retrieve so older tweets")
+        return thread
+    print("downloaded required tweets")
+    startIndex = allTillThread.index(res.id)
+    print("Finding useful tweets")
+    quietLong = 0
+    while startIndex!=0 and quietLong<25:
+        nowIndex = startIndex-1
+        nowTweet = api.get_status(allTillThread[nowIndex], tweet_mode='extended')
+        if nowTweet.in_reply_to_status_id == thread[-1].id:
+            quietLong = 0
+            #print("Reached a useful tweet to be included in thread")
+            thread.append(nowTweet)
+        else:
+            quietLong = quietLong + 1
+        startIndex = nowIndex
+    return thread
 
+def getAllTweetsInThreadBeforeThis(tweetId):
+    thread = []
+    hasReply = True
+    res = api.get_status(tweetId, tweet_mode='extended')
+    while res.in_reply_to_status_id is not None:
+        res = api.get_status(res.in_reply_to_status_id, tweet_mode='extended')
+        thread.append(res)
+    return thread[::-1]
+
+def getAllTweetsInThread(tweetId):
+    tweetsAll = []
+    print("Getting all tweets before this tweet")
+    tweetsAll = getAllTweetsInThreadBeforeThis(tweetId)
+    print(len(tweetsAll))
+    print("Getting all tweets after this tweet")
+    tweetsAll.extend(getAllTweetsInThreadAfterThis(tweetId))
+    return tweetsAll
+
+def printAllTweet(tweets):
+    if len(tweets)>0:
+        print("Thread Messages include:-")
+        for tweetId in range(len(tweets)):
+            print(str(tweetId+1)+". "+tweets[tweetId].full_text)
+            print("")
+    else:
+        print("No Tweet to print")
+
+########################################################################################################
+########### COPIED FROM CSDOJO TWITTER BOT CODE ##########################################################################
+########################################################################################################
+# This section just has basic functions for read and write
 
 FILE_NAME = 'last_seen_id.txt'
 
@@ -30,7 +104,7 @@ def retrieve_last_seen_id(file_name):
     return last_seen_id
 ######################################################
 
-######################################################
+####### STORES LAST SEEN ID #######################
 def store_last_seen_id(last_seen_id, file_name):
     f_write = open(file_name, 'w')
     f_write.write(str(last_seen_id))
@@ -39,31 +113,29 @@ def store_last_seen_id(last_seen_id, file_name):
 ######################################################
 
 ######################################################
-def reply_to_tweets():
-    print('retrieving and replying to tweets...', flush=True)
-    # DEV NOTE: use 1060651988453654528 for testing.
+last_seen_id = 0
+def handle_last_seen_id():   
+    global last_seen_id
     last_seen_id = retrieve_last_seen_id(FILE_NAME)
     # NOTE: We need to use tweet_mode='extended' below to show
     # all full tweets (with full_text). Without it, long tweets
     # would be cut off.
     mentions = api.mentions_timeline(
-                        last_seen_id,
+                        last_seen_id,  # Last seen ID is an int
                         tweet_mode='extended')
     
     for mention in reversed(mentions):
-        print(str(mention.id) + ' - ' + mention.full_text, flush=True)
         last_seen_id = mention.id
         store_last_seen_id(last_seen_id, FILE_NAME)
-        ######getting status object
-        status = api.get_status(last_seen_id)
-        ###########################
-        print('found #helloworld!', flush=True)
-        print('responding back...', flush=True)
-        api.update_status('@' + mention.user.screen_name + status.text +
-                ' back to you!', mention.id)
+    allTweets = getAllTweetsInThread(last_seen_id)
+    printAllTweet(allTweets)
+        
+########################################################################################################
+########################################################################################################
+########################################################################################################
 
-####################################################
 
-while True:
-    reply_to_tweets()
-    time.sleep(15)
+
+while __name__ == '__main__':
+    handle_last_seen_id()
+    time.sleep(2)
