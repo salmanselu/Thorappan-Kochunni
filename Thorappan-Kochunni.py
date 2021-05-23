4d75a4fcf8143b2554af5a49907ca5810201562b
Salman Nazeer <salmanselu2002@gmail.com>
2021-05-23 12:25:35 +0530

    preparing for deploying to heroku

--- Thorappan-Kochunni.py
+++ Thorappan-Kochunni.py
@@ -1,10 +1,11 @@
-import tweepy, time
-
+import tweepy
+import time
+from os import environ
 # Enter your keys/secrets as strings in the following fields
-CONSUMER_KEY = ""
-CONSUMER_SECRET = ""
-ACCESS_KEY = ""
-ACCESS_SECRET = ""
+CONSUMER_KEY = environ['CONSUMER_KEY']
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_KEY = environ['ACCESS_KEY']
+ACCESS_SECRET = environ['ACCESS_SECRET']
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
